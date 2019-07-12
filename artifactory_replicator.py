a2cdf8840767783e373a5074e095aa62ab5cff9b
Alex Talbot <alexander.talbot@nuance.com>
2019-07-12 11:39:41 +0100

    .

--- artifactory_replicator.py
+++ artifactory_replicator.py
@@ -273,23 +273,9 @@ class Repository:
       else:
         self.file_count += 1
 
-
 if __name__ == '__main__':
 
-  # Configure logger.
-  logger = logging.getLogger('artifactory_replicator')
-  logger_level = logging.DEBUG
-  logger.setLevel(logger_level)
-  fh = logging.FileHandler('artifactory_replicator.log')
-  fh.setLevel(logger_level)
-  ch = logging.StreamHandler()
-  ch.setLevel(logger_level)
-  formatter = logging.Formatter('%(asctime)s [%(name)s-%(process)d] [%(levelname)s] - %(message)s')
-  fh.setFormatter(formatter)
-  ch.setFormatter(formatter)
-  logger.addHandler(fh)
-  logger.addHandler(ch)
-
+  
 
   # Load arguments
   parser = argparse.ArgumentParser(description='Run Artifactory repository replication.')
@@ -299,32 +285,40 @@ if __name__ == '__main__':
                      help='The configuration file to load')
 
   parser.add_argument('--source-username',
+                     default=None,
                      help='The username to use to replicate from the source repository')
   parser.add_argument('--source-password',
+                     default=None,
                      help='The password to use to replicate from the source repository')
   parser.add_argument('--source-base-url',
+                     default=None,
                      help='''The base URL to use to replicate from the source
                              repository, e.g.
                              https://artifactory.source.com/artifactory''')
   parser.add_argument('--source-repository-name',
+                     default=None,
                      help='The source repository name')
 
   parser.add_argument('--destination-username',
+                     default=None,
                      help='The username to use to replicate to the destination repository')
   parser.add_argument('--destination-password',
+                     default=None,
                      help='The password to use to replicate to the destination repository')
   parser.add_argument('--destination-base-url',
+                     default=None,
                      help='''The base URL to use to replicate to the destination
                              repository, e.g.
                              https://artifactory.destination.com/artifactory''')
   parser.add_argument('--destination-repository-name',
+                     default=None,
                      help='The destination repository name')
 
 
   parser.add_argument('--keep-temp-directory',
                      default=False,
                      action='store_true',
-                     help='Keep temporary directories (default: False)')
+                     help='Keep temporary directories (default: Delete)')
   parser.add_argument('--max-concurrent-threads',
                      default=10,
                      type=int,
@@ -333,64 +327,105 @@ if __name__ == '__main__':
   parser.add_argument('--no-verify-ssl',
                      default=False,
                      action='store_true',
-                     help='Disable SSL verification (default: False)')
+                     help='Disable SSL verification (default: verify)')
+
+  parser.add_argument('--logging-path',
+                     default='/var/log/artifactory_replicator/artifactory_replicator.log',
+                     help='Path/file to log to (default: /var/log/artifactory_replicator/artifactory_replicator.log)')
+
+  parser.add_argument('--debug',
+                     default=False,
+                     help='Enable debug logging (default: console - none, log - info )')
+
+  parser.add_argument('--console',
+                     default=False,
+                     help='Enable console logging')
 
   parser.add_argument('--version', action='version', version='%(prog)s 0.2.0')
 
   args = parser.parse_args()
 
 
+  # Configure logger.
+  logger = logging.getLogger('artifactory_replicator')
+  
+  # Enable debug logging if --debug is passed else default to info level.
+  logger_level = logging.DEBUG if args.debug else logging.INFO
+  logger.setLevel(logger_level)
+  formatter = logging.Formatter('%(asctime)s [%(name)s-%(process)d] [%(levelname)s] - %(message)s')
+
+  # Enable console logging if --console is passed.
+  if args.console:
+    ch = logging.StreamHandler()
+    ch.setLevel(logger_level)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
   # Load configs
   config_manager = None
+  # Assign arguments provided or argument defaults as config defaults. This
+  # approach treats the config file as the ultimate source of truth ignoring
+  # command line arguments where configs are set.
+  defaults = { 'source_username': args.source_username,
+               'source_password': args.source_password,
+               'source_base_url': args.source_base_ur,
+               'source_repository_name': args.source_repository_name,
+               'destination_username': args.destination_username,
+               'destination_password': args.destination_password,
+               'destination_base_url': args.destination_base_url,
+               'destination_repository_name': args.destination_repository_name,
+               # Inverse logic set for improved readability of
+               # command line argument.
+               'delete_temp_directory': not args.keep_temp_directory,
+               'max_concurrent_threads': args.max_concurrent_threads,
+               'verify_ssl': args.no_verify_ssl,
+               'logging_path': args.logging_path }
+
   try:
-    defaults = { 'delete_temp_directory': 'false',
-                 'max_concurrent_threads': 10,
-                 'verify_ssl': True }
-
-    required = [ 'source_username',
-                 'source_password',
-                 'source_base_url',
-                 'source_repository_name',
-                 'destination_username',
-                 'destination_password',
-                 'destination_base_url',
-                 'destination_repository_name' ]
-
     logger.info('Loading configuration ' + args.config)
     config_manager = ConfigManager(config_file_path = args.config, defaults = defaults, required = required)
     logger.info('Configuration loaded')
 
+    # todo - set log level from config.
+    # Configure file logging.
+    fh = logging.FileHandler(config_manager['logging_path'])
+    fh.setLevel(logger_level)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
   except (OSError, IOError) as exception:
-    logger.error('Abondoning config load')
+    logger.error('Unable to load config')
 
-  else:
-    # Repository instances representative of the source and destination repositories.
-    source_repository      = Repository( username               = config_manager['source_username'],
-                                         password               = config_manager['source_password'],
-                                         repository_name        = config_manager['source_repository_name'],
-                                         base_url               = config_manager['source_base_url'],
-                                         delete_temp_directory  = config_manager['delete_temp_directory'],
-                                         max_concurrent_threads = config_manager['max_concurrent_threads'],
-                                         #filename = '/tmp/artifactory_replicator-b8eJCc/example_repository-local.json',
-                                         verify_ssl             = config_manager['verify_ssl'] )
-    destination_repository = Repository( username               = config_manager['destination_username'],
-                                         password               = config_manager['destination_password'],
-                                         repository_name        = config_manager['destination_repository_name'],
-                                         base_url               = config_manager['destination_base_url'],
-                                         delete_temp_directory  = config_manager['delete_temp_directory'],
-                                         max_concurrent_threads = config_manager['max_concurrent_threads'],
-                                         #filename = '/tmp/artifactory_replicator-Nayx5r/example_repository-remote.json',
-                                         verify_ssl             = config_manager['verify_ssl'] )
 
-    try:
-      # Pass a source repository reference to the destination instance.
-      destination_repository.set_source_repository(source_repository)
-    except TypeError as exception:
-        logger.error(str(exception))
-    else:
-      logger.info('Initiating replication')
-      try:
-        destination_repository.run_replication()
-      except Exception as exception:
-        logger.error(str(exception))
+#  else:
+#    # Repository instances representative of the source and destination repositories.
+#    source_repository      = Repository( username               = config_manager['source_username'],
+#                                         password               = config_manager['source_password'],
+#                                         repository_name        = config_manager['source_repository_name'],
+#                                         base_url               = config_manager['source_base_url'],
+#                                         delete_temp_directory  = config_manager['delete_temp_directory'],
+#                                         max_concurrent_threads = config_manager['max_concurrent_threads'],
+#                                         #filename = '/tmp/artifactory_replicator-b8eJCc/example_repository-local.json',
+#                                         verify_ssl             = config_manager['verify_ssl'] )
+#    destination_repository = Repository( username               = config_manager['destination_username'],
+#                                         password               = config_manager['destination_password'],
+#                                         repository_name        = config_manager['destination_repository_name'],
+#                                         base_url               = config_manager['destination_base_url'],
+#                                         delete_temp_directory  = config_manager['delete_temp_directory'],
+#                                         max_concurrent_threads = config_manager['max_concurrent_threads'],
+#                                         #filename = '/tmp/artifactory_replicator-Nayx5r/example_repository-remote.json',
+#                                         verify_ssl             = config_manager['verify_ssl'] )
+#
+#    try:
+#      # Pass a source repository reference to the destination instance.
+#      destination_repository.set_source_repository(source_repository)
+#    except TypeError as exception:
+#        logger.error(str(exception))
+#    else:
+#      logger.info('Initiating replication')
+#      try:
+#        destination_repository.run_replication()
+#      except Exception as exception:
+#        logger.error(str(exception))
 
